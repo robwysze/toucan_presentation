12571e1174687c88eb72f1645591b4295c4e5028
Robert Wyszecki <r.wyszecki@toucan-systems.pl>
2021-05-24 12:43:25 +0200

    test black

--- toucan_presentation/main.py
+++ toucan_presentation/main.py
@@ -3,8 +3,8 @@ from person import Person
 
 def main():
     new_person = Person(name="Ibarbo", age=31)
-    quot = new_person.introduce()
-    print(quot)
+    quote = new_person.introduce()
+    print(quote)
 
 
 if __name__ == '__main__':
