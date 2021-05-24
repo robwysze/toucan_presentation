3a9c22c98bec741d9d637d8e669cb2e1a386aafd
Robert Wyszecki <r.wyszecki@toucan-systems.pl>
2021-05-24 12:49:13 +0200

    black formatting

--- toucan_presentation/main.py
+++ toucan_presentation/main.py
@@ -3,9 +3,9 @@ from person import Person
 
 def main():
     new_person = Person(name="Ibarbo", age=31)
-    quote = new_person.introduce()
-    print(quote)
+    quot = new_person.introduce()
+    print(quot)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
